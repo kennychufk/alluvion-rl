fix action scaling for learning

ddpg_torch.py
@@ -57,6 +57,9 @@ class ReplayBuffer:
         self.rew[self.ptr] = rew
         self.obs1[self.ptr] = obs1
         self.term[self.ptr] = 1 - done
+        if (self.size % 1000 == 0):
+            print('obs', obs0, obs1)
+            print('act rew term', act, rew, self.term[self.ptr])
         self.ptr = (self.ptr + 1) % self.capacity
         self.size = min(self.size + 1, self.capacity)
 
@@ -191,10 +194,10 @@ class MLPActor(nn.Module):
         final_layer.bias.data.mul_(final_layer_scale)
 
     def forward(self, state):
-        x = self.net(state)
-        return torch.Tensor(self.action_half_extent).to(
-            x.get_device()) * x + torch.Tensor(self.action_mean).to(
-                x.get_device())
+        return self.net(state)
+
+    def from_normalized_action(self, act):
+        return self.action_half_extent * act + self.action_mean
 
 
 class TD3:
@@ -262,8 +265,7 @@ class TD3:
         DDPGAgent.hard_update(self.target_critic, self.critic)
 
     def uniform_random_action(self):
-        return np.random.uniform(self.actor.min_action, self.actor.max_action,
-                                 self.act_dim)
+        return np.random.uniform(-1, 1, self.act_dim)
 
     def get_action(self, observation, enable_noise=True):
         self.actor.eval()
@@ -271,10 +273,8 @@ class TD3:
                                    dtype=torch.float).to(torch.device('cuda'))
         mu = self.actor.forward(observation).cpu().detach().numpy()
         if enable_noise:
-            mu += self.expl_noise_func(
-                self.act_dim
-            ) * self.actor.action_half_extent + self.actor.action_mean
-            np.clip(mu, self.actor.min_action, self.actor.max_action, out=mu)
+            mu += self.expl_noise_func(self.act_dim)
+            np.clip(mu, -1, 1, out=mu)
         self.actor.train()
         return mu
 
@@ -294,9 +294,7 @@ class TD3:
         with torch.no_grad():
             noise = (torch.randn_like(action) * self.policy_noise).clamp(
                 -self.noise_clip, self.noise_clip)
-            new_action = (self.target_actor(new_state) + noise).clamp(
-                torch.Tensor(self.actor.min_action).to(action.get_device()),
-                torch.Tensor(self.actor.max_action).to(action.get_device()))
+            new_action = (self.target_actor(new_state) + noise).clamp(-1, 1)
 
             target_q0, target_q1 = self.target_critic(new_state, new_action)
             target_q = torch.min(target_q0, target_q1)
@@ -431,8 +429,7 @@ class DDPGAgent:
         DDPGAgent.hard_update(self.target_critic, self.critic)
 
     def uniform_random_action(self):
-        return np.random.uniform(self.actor.min_action, self.actor.max_action,
-                                 self.act_dim)
+        return np.random.uniform(-1, 1, self.act_dim)
 
     def get_action(self, observation, enable_noise=True):
         self.actor.eval()
@@ -440,10 +437,8 @@ class DDPGAgent:
                                    dtype=torch.float).to(torch.device('cuda'))
         mu = self.actor.forward(observation).cpu().detach().numpy()
         if enable_noise:
-            mu += self.expl_noise_func(
-                self.act_dim
-            ) * self.actor.action_half_extent + self.actor.action_mean
-            np.clip(mu, self.actor.min_action, self.actor.max_action, out=mu)
+            mu += self.expl_noise_func(self.act_dim)
+            np.clip(mu, -1, 1, out=mu)
         self.actor.train()
         return mu
 

lunar-lander-train.py
@@ -74,12 +74,12 @@ while True:
         new_state, reward, done, info = env.step(act)
         agent.remember(obs, act, reward, new_state, int(done))
         agent.learn()
+        sample_step += 1
         score += reward
         obs = new_state
         # env.render()
     score_history.append(score)
 
-    sample_step += 1
     if i % 50 == 0:
         agent.save_models(wandb.run.dir)
     wandb.log({'score': score, 'score100': np.mean(list(score_history))})

playground.py
@@ -281,23 +281,24 @@ truth_buoy_pile_real = dp.Pile(dp, runner, 0)
 for i in range(num_buoys):
     truth_buoy_pile_real.add(dp.SphereDistance.create(0), al.uint3(64, 64, 64))
 
-agent = DDPGAgent(
-    actor_lr=1e-5,
-    critic_lr=1e-4,
-    critic_weight_decay=1e-2,
+agent = TD3(
+    actor_lr=3e-4,
+    critic_lr=3e-4,
+    critic_weight_decay=0,
     obs_dim=33,
     # act_dim=21,
     act_dim=1,
-    expl_noise_func=GaussianNoise(std_dev=0.0),
+    expl_noise_func=GaussianNoise(std_dev=0.1),
     gamma=0.95,
     # hidden_sizes=[2048, 1800],
-    min_action=np.array([-100]),
-    max_action=np.array([100]),
-    hidden_sizes=[400, 300],
-    actor_final_scale=0.25,
-    critic_final_scale=0.25,
-    soft_update_rate=0.001,
-    batch_size=64)
+    min_action=np.array([0]),
+    max_action=np.array([1000]),
+    learn_after=100000,
+    hidden_sizes=[600, 400],
+    actor_final_scale=1,
+    critic_final_scale=1,
+    soft_update_rate=0.005,
+    batch_size=256)
 wandb.init(project='alluvion-rl')
 config = wandb.config
 config.actor_lr = agent.actor_lr
@@ -321,6 +322,7 @@ wandb.watch(agent.critic)
 
 with open('switch', 'w') as f:
     f.write('1')
+sample_step = 0
 while True:
     with open('switch', 'r') as f:
         if f.read(1) == '0':
@@ -373,14 +375,19 @@ while True:
             dp.coat(truth_buoy_pile_real.x).get())
         usher_sampling.sample_x.set(truth_buoy_x_np)
 
-        act_aggregated = agent.get_action(obs_aggregated, enable_noise=True)
+        if sample_step < agent.learn_after:
+            act_aggregated = np.zeros((num_buoys, agent.act_dim))
+            for buoy_id in range(num_buoys):
+                act_aggregated[buoy_id] = agent.uniform_random_action()
+        else:
+            act_aggregated = agent.get_action(obs_aggregated)
         if frame_id % 100 == 0:
             print(frame_id, act_aggregated)
         if np.sum(np.isnan(act_aggregated)) > 0:
             print(obs_aggregated, act_aggregated)
             sys.exit(0)
         set_usher_param(solver.usher, dp, unit, truth_buoy_pile_real,
-                        act_aggregated)
+                        agent.actor.from_normalized_action(act_aggregated))
         dp.map_graphical_pointers()
         while (solver.t < target_t):
             solver.step()
@@ -414,11 +421,12 @@ while True:
 
         for buoy_id in range(num_buoys):
             agent.remember(
-                obs_aggregated[buoy_id], act_aggregated[buoy_id],
-                (reward - do_nothing_reward) * 100,
+                obs_aggregated[buoy_id], act_aggregated[buoy_id], reward * 10,
                 new_obs_aggregated[buoy_id],
                 int(early_termination or frame_id == (num_frames - 2)))
-        agent.learn()
+        if sample_step >= agent.learn_after:  # as memory size is num_buoys * sample_step
+            agent.learn()
+        sample_step += 1
         score += reward
         do_nothing_score += do_nothing_reward
         obs_aggregated = new_obs_aggregated