e59498eb846168479ba765280160225de640d536
Kenny CHU <kennychu@riec.tohoku.ac.jp>
2021-12-04 18:03:01 +0900

    add piv post-processing

--- piv-mat2np.py
+++ piv-mat2np.py
@@ -3,54 +3,34 @@ import scipy.io as sio
 import sys
 
 
-def uv_mat_to_np(u_mat, v_mat, row_range):
+def uv_mat_to_np(u_mat, v_mat):
     uv_np = np.zeros(
-        [len(u_mat), (row_range[1] - row_range[0]),
+        [len(u_mat), len(u_mat[0][0]),
          len(u_mat[0][0][0]), 2], np.float64)
     for time_id in range(len(u_mat)):
         uv_np[time_id] = uv = np.dstack([
-            u_mat[time_id][0][row_range[0]:row_range[1]].astype(np.float64),
-            v_mat[time_id][0][row_range[0]:row_range[1]].astype(np.float64)
+            u_mat[time_id][0].astype(np.float64),
+            v_mat[time_id][0].astype(np.float64)
         ])
     return uv_np
 
 
-def read_convert_uv_mat(containing_dir, tag, row_range):
+def read_convert_uv_mat(containing_dir, tag):
     u_mat = np.array(sio.loadmat(f'{containing_dir}/u_{tag}.mat')[f'u_{tag}'])
     v_mat = np.array(sio.loadmat(f'{containing_dir}/v_{tag}.mat')[f'v_{tag}'])
-    return uv_mat_to_np(u_mat, v_mat, row_range)
+    return uv_mat_to_np(u_mat, v_mat)
 
 
-def read_convert_xy_mat(containing_dir, row_range):
+def read_convert_xy_mat(containing_dir):
     x_mat = np.array(sio.loadmat(f'{containing_dir}/x.mat')['x'])
     y_mat = np.array(sio.loadmat(f'{containing_dir}/y.mat')['y'])
-    return np.dstack([
-        x_mat[0][0][row_range[0]:row_range[1]],
-        y_mat[0][0][row_range[0]:row_range[1]]
-    ]).astype(np.float64)
-
-
-def read_row_range(containing_dir):
-    typevector = np.array(
-        sio.loadmat(f'{containing_dir}/typevector_original.mat')
-        ['typevector_original'])
-    mask = typevector[0][0]
-    start_row_id = -1
-    end_row_id = -1
-    for row_id, row in enumerate(mask):
-        if start_row_id < 0 and (np.sum(row) == len(row)):
-            start_row_id = row_id
-        elif start_row_id >= 0 and (np.sum(row) < len(row)):
-            end_row_id = row_id
-            break
-    return (start_row_id, end_row_id)
+    return np.dstack([x_mat[0][0], y_mat[0][0]]).astype(np.float64)
 
 
 def read_and_export_uv_xy(containing_dir, tag):
-    row_range = read_row_range(containing_dir)
-    uv = read_convert_uv_mat(containing_dir, tag, row_range)
+    uv = read_convert_uv_mat(containing_dir, tag)
     np.save(f'{containing_dir}/vel_{tag}', uv)
-    xy = read_convert_xy_mat(containing_dir, row_range)
+    xy = read_convert_xy_mat(containing_dir)
     np.save(f'{containing_dir}/pos', xy)
 
 
--- piv-overlay-type.py
+++ piv-overlay-type.py
@@ -3,6 +3,7 @@ import sys
 import shutil
 import glob
 import numpy as np
+from numpy import linalg as LA
 import subprocess
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -11,22 +12,14 @@ import matplotlib as mpl
 import scipy.io as sio
 import seaborn as sns
 import matplotlib
-import matplotlib
+import pandas as pd
 from joblib import Parallel, delayed
 matplotlib.use('Agg')
-
-
-def read_typevector_filtered(containing_dir):
-    return np.array(
-        sio.loadmat(f'{containing_dir}/mat_results/typevector_filtered.mat')
-        ['typevector_filtered'])
-
 
 piv_dir_name = sys.argv[1]
 containing_dir = f'/media/kennychufk/vol1bk0/{piv_dir_name}/'
 pos = np.load(f'{containing_dir}/mat_results/pos.npy')
 vel = np.load(f'{containing_dir}/mat_results/vel_filtered.npy')
-typevector_filtered = read_typevector_filtered(containing_dir)
 
 image_dim = 1024
 calxy = np.load(f'{containing_dir}/calxy.npy').item()
@@ -34,38 +27,88 @@ offset_x = np.load(f'{containing_dir}/offset_x.npy').item()
 offset_y = np.load(f'{containing_dir}/offset_y.npy').item()
 num_points_per_row = pos.shape[1]
 pix_point_interval = image_dim / (num_points_per_row + 1)
+pos_x = pos[..., 0]
 pix_x = (pos[..., 0] + offset_x) / calxy
 pix_y = image_dim + ((-pos[..., 1] - offset_y) / calxy)
+robot_filename = glob.glob(f'{containing_dir}/Trace*.csv')[0]
+robot_frames = pd.read_csv(
+    robot_filename,
+    comment='"',
+    names=['tid', 'j0', 'j1', 'j2', 'j3', 'j4', 'j5', 'x', 'y', 'z', 'v'])
+agitator_x = robot_frames['y'].to_numpy() / 1000.0
 
 frame_prefix = piv_dir_name
 
 cmap = sns.color_palette("vlag", as_cmap=True)
 
+piv_freq = 500.0
+robot_freq = 200.0
 
-def render_field(containing_dir, frame_prefix, frame_id, pix_x, pix_y,
-                 typevector_filtered, cmap):
+
+def render_field(containing_dir, frame_prefix, frame_id, pix_x, pix_y, cmap):
     my_dpi = 128
-    source_img = f"{containing_dir}/{frame_prefix}{frame_id+1:06d}.tif"
+    robot_tid = int(frame_id / piv_freq * robot_freq)
+    if robot_tid >= 4000:
+        robot_tid = 3999
+    source_img_filename = f"{containing_dir}/{frame_prefix}{frame_id+1:06d}.tif"
     fig = plt.figure(figsize=[image_dim // my_dpi, image_dim // my_dpi],
                      dpi=my_dpi,
                      frameon=False)
     img_ax = plt.Axes(fig, [0., 0., 1., 1.])
     fig.add_axes(img_ax)
+    source_img = plt.imread(source_img_filename)
 
-    img_ax.imshow(plt.imread(source_img), cmap='gray')
+    window_size = 44
+    analyze_x0 = int(pix_x[0][0]) - window_size // 2
+    analyze_x1 = int(pix_x[0][-1]) + window_size // 2
+    analyze_y0 = int(pix_y[0][0]) - window_size // 2
+    analyze_y1 = int(pix_y[-1][0]) + window_size // 2
+
+    num_windows_x = (analyze_x1 - analyze_x0) // window_size
+    num_windows_y = (analyze_y1 - analyze_y0) // window_size
+
+    analyze_img = np.array(
+        source_img[analyze_y0:analyze_y1, analyze_x0:analyze_x1] / 4096,
+        dtype=np.float32)
+    #     img_ax.imshow(analyze_img, cmap='gray')
+    #     img_ax.set_axis_off()
+
+    img_ax.imshow(source_img, cmap='gray')
     img_ax.set_axis_off()
-    u = vel[frame_id, ..., 0]
-    v = vel[frame_id, ..., 1]
+    u = np.copy(vel[frame_id, ..., 0])
+    v = np.copy(vel[frame_id, ..., 1])
+    acceleration_mask = np.ones(u.shape, dtype=bool)
+    if frame_id > 0:
+        vel_diff = vel[frame_id] - vel[frame_id - 1]
+        acceleration_mask = LA.norm(vel_diff, axis=2) < 0.08
+
+    velocity_mask = LA.norm(vel[frame_id], axis=2) < 0.18
+    agitator_mask = np.abs(pos_x - agitator_x[robot_tid]) > 0.012
+
+    brightness_mask = np.ones(u.shape, dtype=bool)
+    for window_x in range(num_windows_x):
+        for window_y in range(num_windows_y):
+            brightness = np.mean(
+                analyze_img[window_y * window_size:(window_y + 1) *
+                            window_size, window_x *
+                            window_size:(window_x + 1) * window_size])
+            brightness_mask[window_y, window_x] = (brightness < 0.1)
+
+    mask = agitator_mask & brightness_mask & velocity_mask & acceleration_mask
     mag = np.hypot(u, v)
-    is_filtered = typevector_filtered[frame_id][0] - 1
-    q = img_ax.quiver(pix_x, pix_y, u, v, is_filtered, cmap=cmap, scale=5)
+    q = img_ax.quiver(pix_x[mask],
+                      pix_y[mask],
+                      u[mask],
+                      v[mask],
+                      mag[mask],
+                      cmap=cmap,
+                      scale=5)
     fig.savefig(f'{containing_dir}/gridfield/{frame_id}.png', dpi=my_dpi)
     plt.close('all')
 
 
-os.mkdir(f'{containing_dir}/gridfield')
+os.makedirs(f'{containing_dir}/gridfield', exist_ok=True)
 num_frames = len(vel)
-Parallel(n_jobs=8)(
-    delayed(render_field)(containing_dir, frame_prefix, frame_id, pix_x, pix_y,
-                          typevector_filtered, cmap)
-    for frame_id in range(num_frames))
+Parallel(n_jobs=8)(delayed(render_field)(containing_dir, frame_prefix,
+                                         frame_id, pix_x, pix_y, cmap)
+                   for frame_id in range(num_frames))
