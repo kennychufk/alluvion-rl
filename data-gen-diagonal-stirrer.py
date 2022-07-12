fix quaternion order bug and use noised buoy interpolators

data-gen-diagonal-stirrer.py
@@ -55,6 +55,17 @@ cni.max_num_particles_per_cell = 64
 cni.max_num_neighbors_per_particle = 64
 
 agitator_option = 'stirrer/stirrer'
+# agitator_options = [
+#     'bunny/bunny',
+#     '03797390/ec846432f3ebedf0a6f32a8797e3b9e9',
+#     '03046257/757fd88d3ddca2403406473757712946',
+#     '02942699/9db4b2c19c858a36eb34db531a289b8e',
+#     '03261776/1d4f9c324d6388a9b904f4192b538029',
+#     '03325088/daa26435e1603392b7a867e9b35a1295',
+#     '03513137/91c0193d38f0c5338c9affdacaf55648',
+# ]
+# agitator_option = agitator_options[np.random.randint(
+#     low=0, high=len(agitator_options))]
 
 agitator_model_dir = f'{args.shape_dir}/{agitator_option}/models'
 agitator_mesh_filename = f'{agitator_model_dir}/manifold2-decimate-2to-8.obj'
@@ -169,7 +180,8 @@ agitator_id = pile.add_pellets(agitator_distance,
                                friction=0.3,
                                inertia_tensor=agitator_inertia,
                                display_mesh=agitator_mesh)
-exp_dir = '/media/kennychufk/vol1bk0/20210415_162749-laser-too-high/'
+exp_dir = '/media/kennychufk/vol1bk0/20210415_162749-laser-too-high/' # diagonal stir
+# exp_dir = '/media/kennychufk/vol1bk0/20210416_114327/' # diagonal & circular
 
 
 def transform_robot_position(pos_real):
@@ -179,8 +191,14 @@ def transform_robot_position(pos_real):
 
 
 interpolator = RigidInterpolator(dp, unit, f'{exp_dir}/Trace.csv')
+# NOTE: for stirrer
+shift_for_centered_agitator = dp.f3(0, 0, 0)
 pile.x[agitator_id] = unit.from_real_length(
     transform_robot_position(interpolator.get_x_real_from_real_t(0)))
+# # NOTE: for centered agitator
+# shift_for_centered_agitator = dp.f3(0, -0.19, 0)
+# pile.x[agitator_id] = unit.from_real_length(
+#     transform_robot_position(interpolator.get_x_real_from_real_t(0)) + shift_for_centered_agitator)
 dp.remove(agitator_pellet_x)
 for i in range(num_buoys):
     buoy_id = i + 1
@@ -406,7 +424,7 @@ while not rest_state_achieved or solver.t < target_t:
 
             pile.v[agitator_id] = interpolator.get_v(solver.t)
             pile.x[agitator_id] = unit.from_real_length(
-                transform_robot_position(interpolator.get_x_real(solver.t)))
+                transform_robot_position(interpolator.get_x_real(solver.t))+shift_for_centered_agitator)
         else:  # if not rest_state_achieved
             v_rms = np.sqrt(
                 runner.sum(solver.particle_cfl_v2, solver.num_particles) /

fill-obj-container.py
@@ -9,8 +9,9 @@ from scipy.spatial.transform import Rotation as R
 from PIL import Image
 
 import alluvion as al
+import alluvol
 
-from util import Unit, FluidSamplePellets, get_timestamp_and_hash, BuoySpec, parameterize_kinematic_viscosity, RigidInterpolator
+from util import Unit, FluidSamplePellets, get_timestamp_and_hash, BuoySpec, parameterize_kinematic_viscosity_with_pellets, RigidInterpolator
 
 parser = argparse.ArgumentParser(description='RL ground truth generator')
 parser.add_argument('--output-dir', type=str, default='.')
@@ -49,8 +50,7 @@ cn.boundary_epsilon = 1e-9
 cn.gravity = gravity
 kinematic_viscosity_real = 21.9e-3 / density0_real  # TODO: discrete, either water or GWM
 cn.viscosity, cn.boundary_viscosity = unit.from_real_kinematic_viscosity(
-    parameterize_kinematic_viscosity(kinematic_viscosity_real)
-)  # TODO: should change to parameterize_kinematic_viscosity_with_pellets!!!
+    parameterize_kinematic_viscosity_with_pellets(kinematic_viscosity_real))
 cni.max_num_particles_per_cell = 64
 cni.max_num_neighbors_per_particle = 64
 
@@ -61,15 +61,10 @@ agitator_options = [
     '02942699/9db4b2c19c858a36eb34db531a289b8e',
     '03261776/1d4f9c324d6388a9b904f4192b538029',
     '03325088/daa26435e1603392b7a867e9b35a1295',
-    '03759954/35f36e337df50fcb92b3c4741299c1af',
-    # '04401088/e5bb37415abcf3b7e1c1039f91f43fda',
-    # '04530566/66a90b7b92ff2549f2635cfccf45023',
-    # '03513137/91c0193d38f0c5338c9affdacaf55648',
-    # '03636649/83353863ea1349682ebeb1e6a8111f53'
+    '03513137/91c0193d38f0c5338c9affdacaf55648',
 ]
-
-agitator_selected_id = 0
-agitator_option = agitator_options[agitator_selected_id]
+agitator_option = agitator_options[np.random.randint(
+    low=0, high=len(agitator_options))]
 
 agitator_model_dir = f'{args.shape_dir}/{agitator_option}/models'
 agitator_mesh_filename = f'{agitator_model_dir}/manifold2-decimate-2to-8.obj'
@@ -150,12 +145,6 @@ for i in range(num_buoys):
                                    container_distance.aabb_max),
                                q=dp.f4(0, 0, 0, 1),
                                display_mesh=buoy.mesh)
-    while (has_collision(pile, buoy_id)):
-        print('has collision', pile.x[buoy_id])
-        pile.x[buoy_id] = get_random_position(container_distance.aabb_min,
-                                              container_distance.aabb_max)
-for i in range(num_buoys):
-    print(f"{i+1}: {pile.x[i+1]}")
 dp.remove(buoy_pellet_x)
 pile.hint_identical_sequence(1, pile.get_size())
 
@@ -194,11 +183,16 @@ agitator_id = pile.add_pellets(agitator_distance,
                                    container_distance.aabb_max),
                                q=get_random_quat(),
                                display_mesh=agitator_mesh)
-dp.remove(agitator_pellet_x)
-while has_collision(pile, agitator_id):
+while (has_collision(pile, agitator_id)):
     pile.x[agitator_id] = get_random_position(container_distance.aabb_min,
                                               container_distance.aabb_max)
-    pile.q[agitator_id] = get_random_quat()
+dp.remove(agitator_pellet_x)
+for i in range(num_buoys):
+    buoy_id = i + 1
+    while (has_collision(pile, buoy_id)):
+        print('has collision', pile.x[buoy_id])
+        pile.x[buoy_id] = get_random_position(container_distance.aabb_min,
+                                              container_distance.aabb_max)
 
 pile.reallocate_kinematics_on_device()
 pile.set_gravity(gravity)
@@ -234,7 +228,8 @@ num_samples = pile.compute_sort_fluid_block_internal_all(
     particle_radius=sample_radius,
     mode=sample_x_fill_mode)
 print('num_samples', num_samples)
-sampling = FluidSamplePellets(dp, np.zeros((num_samples, 3), dp.default_dtype))
+sampling = FluidSamplePellets(dp, np.zeros((num_samples, 3), dp.default_dtype),
+                              dp.cni)
 runner.launch_create_fluid_block_internal(sampling.sample_x,
                                           sample_internal_encoded,
                                           num_samples,
@@ -448,7 +443,7 @@ while not rest_state_achieved or solver.t < target_t:
                 last_tranquillized = solver.t
             elif unit.to_real_time(solver.t - last_tranquillized
                                    ) > 0.4 and unit.to_real_velocity(
-                                       v_rms) < 0.030:
+                                       v_rms) < 0.015:
                 print("rest state achieved at", unit.to_real_time(solver.t))
                 solver.t = 0
                 rest_state_achieved = True

util/environment.py
@@ -248,6 +248,29 @@ class Environment:
         self.mask = self.dp.create_coated(
             (self.simulation_sampling.num_samples), 1, np.uint32)
 
+    def reset_buoy_interpolators(self):
+        marker_dtype = np.dtype([('t', np.float32), ('x', np.float32, 3),
+                                 ('q', np.float32, 4)])
+        buoy_trajectories = np.empty((self.num_buoys, self._max_episode_steps),
+                                     dtype=marker_dtype)
+        for episode_t in range(self._max_episode_steps):
+            truth_pile_x, truth_pile_v, truth_pile_q, truth_pile_omega = read_pile(
+                f'{self.truth_dir}/{episode_t}.pile')
+            buoy_trajectories[:, episode_t]['x'] = truth_pile_x[1:1 +
+                                                                self.num_buoys]
+            buoy_trajectories[:, episode_t]['q'] = np.roll(
+                truth_pile_q[1:1 + self.num_buoys], -1, axis=1)
+        for buoy_id in range(self.num_buoys):
+            buoy_trajectories[buoy_id]['x'] += np.tile(
+                np.random.normal(scale=2e-3, size=(3)),
+                self._max_episode_steps).reshape(-1, 3)
+        self.buoy_interpolators = [
+            BuoyInterpolator(self.dp,
+                             sample_interval=0.01,
+                             trajectory=trajectory)
+            for trajectory in buoy_trajectories
+        ]
+
     def reset_solver_properties(self):
         self.unit = Unit(real_kernel_radius=self.unit.rl,
                          real_density0=np.load(
@@ -290,6 +313,7 @@ class Environment:
     def reset(self):
         self.reset_truth_dir()
         self.reset_vectors()
+        self.reset_buoy_interpolators()
         self.reset_solver_properties()
         self.reset_solver_initial()
         self.dp.copy_cn_external(self.cn, self.cni)
@@ -301,10 +325,18 @@ class Environment:
         return state_aggregated
 
     def get_buoy_kinematics_real(self, episode_t):
-        truth_pile_x, truth_pile_v, truth_pile_q, truth_pile_omega = read_pile(
-            f'{self.truth_dir}/{episode_t}.pile')
-        return truth_pile_x[1:1 + self.num_buoys], truth_pile_v[
-            1:1 + self.num_buoys], truth_pile_q[1:1 + self.num_buoys]
+        t_real = episode_t * self.truth_real_interval
+        buoy_v_shift = np.zeros(3, self.dp.default_dtype)
+        buoys_x = np.zeros((self.num_buoys, 3), self.dp.default_dtype)
+        buoys_v = np.zeros((self.num_buoys, 3), self.dp.default_dtype)
+        buoys_q = np.zeros((self.num_buoys, 4), self.dp.default_dtype)
+
+        for buoy_id in range(self.num_buoys):
+            buoys_x[buoy_id] = self.buoy_interpolators[buoy_id].get_x(t_real)
+            buoys_v[buoy_id] = self.buoy_interpolators[buoy_id].get_v(
+                t_real) + buoy_v_shift
+            buoys_q[buoy_id] = self.buoy_interpolators[buoy_id].get_q(t_real)
+        return buoys_x, buoys_v, buoys_q
 
     def collect_state(self, episode_t):
         buoy_x_real, self.buoy_v_real, buoy_q = self.get_buoy_kinematics_real(
@@ -466,6 +498,7 @@ class EnvironmentPIV(Environment):
         self.truth_v_collection[..., 1] = truth_v_piv[..., 1]
         self._max_episode_steps = len(truth_v_piv)
 
+    def reset_buoy_interpolators(self):
         used_buoy_ids = np.load(f'{self.truth_dir}/rec/marker_ids.npy')
         buoy_trajectories = [
             np.load(

util/rigid_interpolator.py
@@ -54,7 +54,12 @@ class BuoyInterpolator:
         self.dp = dp
         times = np.arange(len(trajectory)) * sample_interval
         self.cubic_spline = CubicSpline(times, trajectory['x'])
-        self.slerp = Slerp(times, R.from_quat(trajectory['q']))
+        self.slerp = Slerp(
+            times, R.from_quat(np.roll(trajectory['q'], -1, axis=-1))
+        )  # TODO: change emstrobol output format to prevent similar bugs?
+        # both scipy and alluvion(float4) uses <x, y, z, w>
+        # only penrose/emstrobol uses <w, x, y, z>
+        # TODO: redo offline regression due to the presence of better DNN model params
 
     def get_x(self, t):
         return self.cubic_spline(t)